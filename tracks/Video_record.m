e2603da83f684b3e5d3d2703364a0e6a072d4107
VSD_401 <46393778+vsd401@users.noreply.github.com>
2023-07-03 09:35:37 +0800

    Add files via upload

--- tracks/Video_record.m
+++ tracks/Video_record.m
@@ -1,33 +1,24 @@
 close all
 % initialize variables to store simulation results
 filename= 'Trackinfo.txt';
+NN =  N+1;
+out.mu_x_pred_opt  = NaN(4, NN, size(NMPC_statePred.signals.values,1));         % mean of optimal state prediction sequence
 
-out.mu_x_pred_opt  = NaN(4, N+1, size(NMPC_statePred.signals.values,1));         % mean of optimal state prediction sequence
-s = NaN(N+1,1);
-n = NaN(N+1,1);
-alpha = NaN(N+1,1);
-v = NaN(N+1,1);
-% sss =  reshape(NMPC_statePred.signals.values,251,51,8);
 %提取数据，顺序为s,ey,phi,v,
 % out.u_pred_opt     = NaN(mpc.m, mpc.N,   kmax);         % open-loop optimal input prediction
-for i = 1: size(NMPC_statePred.signals.values,1)
-    % load track
-    for j = 1:N+1
-        % transform data
-        psi
-        s(j) =     NMPC_statePred.signals.values(i,6+(j-1)*size(model.x0,2)); % s的预测值
-        n(j) =     NMPC_statePred.signals.values(i,3+(j-1)*size(model.x0,2)); % n的预测值
-        alpha(j) = NMPC_statePred.signals.values(i,1+(j-1)*size(model.x0,2)); % 这是phi_dot的预测值
-        v(j) =     NMPC_statePred.signals.values(i,2+(j-1)*size(model.x0,2)); % v的预测值
-%         distance=3;
-        [x, y, ~,v] = transformProj2Orig(s, n, alpha, v,filename);
+data_reshape = reshape(NMPC_statePred.signals.values,size(NMPC_statePred.signals.values,1),nx,N+1);
+for i = 1: size(NMPC_statePred.signals.values,1)     %transformProj2Orig(si,ni,alpha,v,filename)
+        S  = reshape(data_reshape(i,6,1:NN),NN,1);
+        NI = reshape(data_reshape(i,5,1:NN),NN,1);
+        ALPHA  = reshape(data_reshape(i,1,1:NN),NN,1);  %actually it's alpha_dot prediction 
+        V = sqrt((reshape(data_reshape(i,2,1:NN),NN,1)).^2+(reshape(data_reshape(i,3,1:NN),NN,1)).^2);
+        [x, y, alpha,v] = transformProj2Orig(S,NI, ALPHA, V,filename);
         out.mu_x_pred_opt(1,:,i)=x;
         out.mu_x_pred_opt(2,:,i)=y;
         out.mu_x_pred_opt(3,:,i)=v;
         out.mu_x_pred_opt(4,:,i)=alpha;
-
-    end
 end
+
 out.mu_x_pred_opt(4,1,:)=psi.Data;
 % start animation
 trackAnim = SingleTrackAnimation(track, out.mu_x_pred_opt);
@@ -38,16 +29,16 @@ for k=2:N %size(NMPC_statePred.signals.values,1)
 %     if ~ trackAnim.updateTrackAnimation(k)
 %         break;
 %     end
-    trackAnim.updateTrackAnimation(k)
+    trackAnim.updateTrackAnimation(k);
 %     trackAnim.updateScope(k);
-    pause(0.15);
+    pause(0.05);
     drawnow;
 end
 
 
 %% Record video
 
-FrameRate = 7;
+FrameRate = 100;
 source_folder = pwd;
 videoName = fullfile(source_folder,sprintf('trackAnimVideo-%s',date));
 videoFormat = 'Motion JPEG AVI';
